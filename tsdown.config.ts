d7b7ff5272610d17a68195e56f58068ab859258d
huanxiaomang <atri2022@163.com>
2025-11-28 10:50:44 +0800

    chore: add icon, format to cjs

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'tsdown'
+import { renameSync } from 'node:fs'
+import { resolve } from 'node:path'
 
 export default defineConfig({
     entry: ['./src/extension.ts'],
     outDir: './out',
     clean: true,
     minify: true,
-    onSuccess: () => {
-        console.log('编译成功')
+    onSuccess() {
+        const oldPath = resolve('./out/extension.cjs')
+        const newPath = resolve('./out/extension.js')
+
+        try {
+            renameSync(oldPath, newPath)
+            console.log('extension.cjs → extension.js')
+        } catch (err) {
+            console.log('can not find cjs file')
+        }
+
     },
+    format: 'cjs',
+
 })
